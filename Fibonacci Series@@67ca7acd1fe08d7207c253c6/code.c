a954c5e9af2669928f8b28a906ddcaf4ad9e7d98
Muskan <muskandev2006@gmail.com>
2025-03-08 18:57:51 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,11 @@
+// Your code here...
+int fibonacciSeries(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    int fib = fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    return fib;
+}
