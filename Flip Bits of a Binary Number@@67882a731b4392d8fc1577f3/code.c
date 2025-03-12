81574e1c389743a19eae12fdada453284e808881
Muskan <muskandev2006@gmail.com>
2025-03-12 13:13:31 +0530

    Solution to Flip Bits of a Binary Number

--- Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
+++ Flip Bits of a Binary Number@@67882a731b4392d8fc1577f3/code.c
@@ -2,21 +2,8 @@
 #include <stdio.h>
 
 int main() {
-    unsigned int num;
-    scanf("%u", &num);
-    for (int i = 31; i >= 0; i--) {  
-        printf("%d", (num >> i) & 1);
-    }
-    printf("\n");
-
-    unsigned int flippedNum = ~num;
-
-    for (int i = 31; i >= 0; i--) {  
-        printf("%d", (flippedNum >> i) & 1);
-    }
-    printf("\n");
-
-    printf("%u\n", flippedNum);
-
+    int num;
+    scanf("%d", &num);  
+    printf("%d\n", ~num);  
     return 0;
 }
