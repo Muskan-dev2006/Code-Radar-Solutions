58a050b32e23cefee92788fad96d80db01175629
Muskan <muskandev2006@gmail.com>
2025-03-20 10:33:19 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -1,19 +1,46 @@
-// Your code here...
+
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
+
     int target;
-    scanf("%d",&target);
-    for(int i=0; i<n; i++){
-        for(int j=1;j<n;j++){
-            if(arr[i]+arr[j]==target){
-                printf("%d %d\n",arr[i],arr[j]);
-            }
+    scanf("%d", &target);
+
+    
+    qsort(arr, n, sizeof(int), compare);
+
+    int left = 0, right = n - 1;
+
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+
+        if (sum == target) {
+            printf("%d %d\n", arr[left], arr[right]);
+
+            
+            while (left < right && arr[left] == arr[left + 1]) left++;
+            while (left < right && arr[right] == arr[right - 1]) right--;
+
+            left++;
+            right--;
+        } else if (sum < target) {
+            left++;  
+        } else {
+            right--;
         }
     }
+
+    return 0;
 }
