e7b6270caacf3be5c0d05f23bbb190ae1f15dd92
Muskan <muskandev2006@gmail.com>
2025-04-01 00:06:50 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -1,47 +1,40 @@
 // Your code here...
 #include <stdio.h>
-
-int findFirstPeak(int arr[], int n) {
-    int isAllSame = 1;
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[i - 1]) {
-            isAllSame = 0;
+int findpeak(int arr[],int n){
+    int allsame = 1;
+    for(int i=0; i<n;i++){
+        if(arr[i]!=arr[i+1]){
+            allsame = 0;
             break;
         }
     }
-    if (isAllSame) return -1; 
-    if (n == 1) return arr[0]; 
-
-    if (arr[0] >= arr[1]) return arr[0]; 
+    if(allsame){
+        return -1;
+    }
+    if(n==1){
+        return arr[0];
+    }
     if(n==2){
-        if(arr[1]>=arr[0]){
+        if(arr[0]<arr[1]){
             return arr[1];
         }
     }
-
-    for (int i = 1; i < n - 1; i++) {
-        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-            return arr[i]; 
+    for(int i=1;i<n-1;i++){
+        if(arr[i]>arr[i+1] && arr[i]<arr[i-1]){
+            return arr[i];
         }
     }
-    if (arr[n-1]>arr[n-2]){
+    if(arr[n-1]>arr[n-2]){
         return arr[n-1];
     }
-
-    return -1; 
 }
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n);
+    scanf("%d",&n);
     int arr[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0; i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    printf("%d", findFirstPeak(arr, n));
-
-    return 0;
+    int peak_element = findpeak(arr,n);
+    printf("%d",peak_element);
 }
